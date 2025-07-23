2edd23bb2d0cf326d09be268bfe11df06b7857f0
Juan Sueldo <78236717+juansueldo@users.noreply.github.com>
2025-07-23 07:08:32 -0300

    Render main.py

--- backend/main.py
+++ backend/main.py
@@ -380,4 +380,4 @@ if __name__ == "__main__":
     print("Iniciando YouTube Downloader API...")
     print("API: http://localhost:8000")
     print("Docs: http://localhost:8000/docs")
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
